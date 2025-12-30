bc99a1645b4797b45eb46f6869d9e413cd0930c8
dhananjayyy09 <djgupta1309@gmail.com>
2025-12-30 18:38:16 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(fn(req, res, next))
+            .catch(next);
+    };
+};
+                                                       
+
+export default asyncHandler
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.status || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error",
+//         })
+//     }
+// }
